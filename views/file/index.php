23c531f4411de60f9fcaca9663c44cfa80a06bec
www <www@lab-sci-304-15.whitworth.edu>
2016-06-14 17:35:23 -0700

    This file was changed from using a gridview to a dynamically styled list of cards. Each card will represent an uploaded file. Each file may be uploaded via the upload button. Multiple files may be uploaded at a time.

--- views/file/index.php
+++ views/file/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\models\File;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\FileSearch */
@@ -9,8 +10,16 @@ use yii\grid\GridView;
 
 $this->title = 'Files';
 $this->params['breadcrumbs'][] = $this->title;
+/*
+$sourcePath = Yii::getAlias('@app') . '/assets/';
+$basePath = Yii::getAlias('@webroot');
+$baseUrl = Yii::getAlias('@web');
+echo "<p>" . $sourcePath . "</p>";
+echo "<p>" . $basePath . "</p>";
+echo "<p>" . $baseUrl . "</p>";
+*/
 ?>
-<div class="file-index">
+<!--- div class="file-index" -->
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -18,7 +27,26 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Upload Files', ['upload/upload'], ['class' => 'btn btn-success']) ?>
     </p>
-    <?= Gridview::widget([
+    <div class='w3-row-padding' style = "max-height:160px">
+        <?php foreach ( $dataProvider->getModels() as $file ): ?>
+            <div class="w3-third w3-margin-bottom">
+                <div class="w3-card-8 w3-center" style="width:80%;">
+                    <div class="w3-container" style="background-image:url(<?php echo $file->getImageurl()?>);
+                        height:10em;
+                        background-size:cover;
+                        background-position:center;
+                        background-repeat:no-repeat;">
+                    </div>
+                    <div class="w3-container w3-center" >
+                        <h4><b><div class="sii-filename-elips"><?php echo $file->orig_name; ?></div> </b></h4>
+                        <p>Tropical Island Group id: 1</p>
+                      </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+<!--     <?= Gridview::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
@@ -37,5 +65,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]); ?> 
+
 </div>
+-->
